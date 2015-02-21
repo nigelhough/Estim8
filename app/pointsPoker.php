29e7fcb565fa02fd24e4760d31f6cb8b011af6d0
Nigel Hough <nigel@nigelhough.co.uk>
2015-02-21 14:58:32 +0000

    Initial class commit

---
+++ app/pointsPoker.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Points Poker
+ *
+ * An Interactive Points Poker tool to help support Agile teams with story estimation
+ *
+ * PHP version 5
+ *
+ * @category Points Poker
+ * @author   Nigel Hough <nigel@nigelhough.co.uk>
+ * @author   Rien Sach <rien.sach@btinternet.com>
+ */
+
+/**
+ * Points Poker Class
+ *
+ * @category Points Poker
+ * @author   Nigel Hough <nigel@nigelhough.co.uk>
+ * @link     TBC
+ */
+class pointsPoker {
+
+    /**
+     * Constructor
+     *
+     */
+    function __construct() {
+
+    }
+
+}
