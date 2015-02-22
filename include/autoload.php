dd12d476529a278b94a81d822ad1ba1ad60a17f5
Tom Szuszai <szuszai@gmail.com>
2015-02-22 19:05:25 +0000

    autoload fixes

--- include/autoload.php
+++ include/autoload.php
@@ -2,6 +2,10 @@
 
 spl_autoload_register(function ($class) {
     if(preg_match("/^votingoptions/i", $class)) {
-        include_once  SITE_ROOT . 'app' . DIRECTORY_SEPARATOR . str_replace("\\",DIRECTORY_SEPARATOR,$class) . '.php';
+        $parts = explode("\\", $class);
+        $className = array_pop($parts);
+        $ds = DIRECTORY_SEPARATOR;
+
+        include_once  SITE_ROOT . 'app' . $ds . strtolower(implode($ds,$parts)) . $ds . $className . '.php';
     }
 });
--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@
     //Set Project Path as a constant
     define('SITE_ROOT',substr(__DIR__,0,strrpos(__DIR__,DIRECTORY_SEPARATOR)+1));
 
-    require_once '../include/autoload.php';
+    require_once SITE_ROOT.'include/autoload.php';
 
     require_once SITE_ROOT.'app/pointsPokerGUI.php';
 
